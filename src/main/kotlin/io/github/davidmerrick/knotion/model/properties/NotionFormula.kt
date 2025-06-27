3302bbb563458efa05b2f4f8f9bc2547a07cc8f1
David Merrick <david@david-merrick.com>
2025-06-27 11:14:25 -0700

    MAJOR: Add boolean for formula

--- src/main/kotlin/io/github/davidmerrick/knotion/model/properties/NotionFormula.kt
+++ src/main/kotlin/io/github/davidmerrick/knotion/model/properties/NotionFormula.kt
@@ -3,5 +3,6 @@ package io.github.davidmerrick.knotion.model.properties
 data class NotionFormula(
     val type: String? = null,
     val string: String? = null,
-    val date: NotionDate? = null
+    val date: NotionDate? = null,
+    val boolean: Boolean? = null
 )
